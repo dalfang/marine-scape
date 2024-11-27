fc4581cbc56558de55385620488095389fd4fa6b
ddalynng <d.fangloy@gmail.com>
2024-11-27 23:33:47 +0100

    adjust gmae speed

--- js/script.js
+++ js/script.js
@@ -1,65 +1,65 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-  const musicToggle = document.getElementById('musicToggle');
+  const musicToggle = document.getElementById("musicToggle");
   const ourGame = new Game();
 
   startButton.addEventListener("click", function () {
-      instructions();
+    instructions();
   });
 
   const closeInstructions = document.getElementById("instructions");
   closeInstructions.addEventListener("click", function () {
-      startGame();
+    startGame();
   });
 
   document.addEventListener("keydown", (event) => {
-      console.log("a key was pressed", event);
-      if (event.code === "ArrowRight") {
-          ourGame.player.element.src = './img/Playerright.png';
-          ourGame.player.directionX = 4;
-      } else if (event.code === "ArrowLeft") {
-          ourGame.player.element.src = './img/Playerleft.png';
-          ourGame.player.directionX = -4;
-      } else if (event.code === "ArrowUp") {
-          ourGame.player.element.src = './img/Playerup.png';
-          ourGame.player.directionY = -4;
-      } else if (event.code === "ArrowDown") {
-          ourGame.player.element.src = './img/Playerdown.png';
-          ourGame.player.directionY = 4;
-      }
+    console.log("a key was pressed", event);
+    if (event.code === "ArrowRight") {
+      ourGame.player.element.src = "./img/Playerright.png";
+      ourGame.player.directionX = 4;
+    } else if (event.code === "ArrowLeft") {
+      ourGame.player.element.src = "./img/Playerleft.png";
+      ourGame.player.directionX = -4;
+    } else if (event.code === "ArrowUp") {
+      ourGame.player.element.src = "./img/Playerup.png";
+      ourGame.player.directionY = -4;
+    } else if (event.code === "ArrowDown") {
+      ourGame.player.element.src = "./img/Playerdown.png";
+      ourGame.player.directionY = 4;
+    }
   });
 
   document.addEventListener("keyup", () => {
-      ourGame.player.directionX = 0;
-      ourGame.player.directionY = 0;
+    ourGame.player.directionX = 0;
+    ourGame.player.directionY = 0;
   });
 
   function instructions() {
-      console.log("to read");
-      ourGame.instructions();
+    console.log("to read");
+    ourGame.instructions();
   }
 
   function startGame() {
-      console.log("start game");
-      ourGame.start();
-      const backgroundMusic = document.getElementById('backgroundMusic');
-      backgroundMusic.play(); 
-      musicToggle.classList.remove('paused');
+    console.log("start game");
+    ourGame.start();
+    const backgroundMusic = document.getElementById("backgroundMusic");
+    backgroundMusic.play();
+    musicToggle.classList.remove("paused");
   }
 
-  musicToggle.addEventListener('click', function() {
-      const backgroundMusic = document.getElementById('backgroundMusic');
-      if (backgroundMusic.paused) {
-          backgroundMusic.play();
-          musicToggle.classList.remove('paused'); // Update to playing image
-        } else {
-            backgroundMusic.pause();
-            musicToggle.classList.add('paused'); // Update to paused image
-        }
-    });
+  musicToggle.addEventListener("click", function () {
+    const backgroundMusic = document.getElementById("backgroundMusic");
+    if (backgroundMusic.paused) {
+      backgroundMusic.play();
+      musicToggle.classList.remove("paused");
+    } else {
+      backgroundMusic.pause();
+      musicToggle.classList.add("paused");
+    }
+  });
 
   restartButton.addEventListener("click", () => {
-      window.location.reload();
+    window.location.reload();
   });
 };
